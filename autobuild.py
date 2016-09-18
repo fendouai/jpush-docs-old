e64b6dffbbd012b997ddec52359bbd43e0d72245
fendouai <fendouai@gmail.com>
2016-09-18 11:34:12 +0800

    test

---
+++ autobuild.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import logging
+import commands
+import os
+import time
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='/opt/push/jpush-docs/autobuild.log',
+                    filemode='a+')
+
+print (os.chdir("/opt/push/jpush-docs/jiguang-docs/"))
+logging.info(commands.getstatusoutput("git add ."))
+logging.info(commands.getstatusoutput('git commit -m "fix the pull to push"'))
+logging.info(commands.getstatusoutput("git push origin renew"))
+
+
+print ("git push origin renew")
+
+
+
+
+
+
